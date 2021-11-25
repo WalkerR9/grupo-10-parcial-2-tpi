7d1a391321564e87e15a55014f6395a1c3ad6779
Moises Molina <mo18022@ues.edu.sv>
2021-11-24 19:45:31 -0600

    Avanzando 1

--- appGrupo10/js/codigo.js
+++ appGrupo10/js/codigo.js
@@ -3,13 +3,13 @@ function obtenerProductos() {
 	fetch('http://127.0.0.1:3000/muebleria')
 		.then(res => res.json())
 		.then(data => {
-			productos =data;
+			productos = data;
 			productos.forEach(
 				function (producto) {
 					producto.precio = parseFloat(producto.precio)
 				});
 			guardar(productos);
-			console.log('Si paso por aqui : tamaño '+productos.length+" la ide: "+productos[2].id);
+			console.log('Si paso por aqui : tamaño ' + productos.length + " la ide: " + productos[2].id);
 			$(document).ready(function () {
 				$('#tabla').DataTable({
 					language: {
@@ -45,7 +45,7 @@ function obtenerProductos() {
 
 function guardar(productos) {
 
-	console.log('Si llego a este metodo: + tamaño: '+productos.length);
+	console.log('Si llego a este metodo: + tamaño: ' + productos.length);
 	for (i = 0; i < productos.length; i++) {
 		var id = productos[i].id;
 		var foto = productos[i].imagen;
@@ -53,15 +53,81 @@ function guardar(productos) {
 		var descripcion = productos[i].descripcion;
 		var categoria = productos[i].categoria;
 		var titulo = productos[i].titulo;
-		let img="<a href=\""+foto+"\" target=\"_blank\"><img src=\""+foto+"\" class=\"img-thumbnail\"  width=\"100\" height=\"100\"></a>"
-		var fila = "<td>" + id + "</td><td>"+img+"</td><td>$" + precio + "</td><td>" + titulo+ "</td><td>" + descripcion + "</td><td>" + categoria + "</td>";
+
+		let boton = "<form action=\"ver.html\" method=\"get\"><input type=\"hidden\" name=\"id\" value=\"" + productos[i].id + " \"><button type=\"submit\" class=\"btn btn-primary\">Ver >></button></form>";
+
+		let img = "<a href=\"" + foto + "\" target=\"_blank\"><img src=\"" + foto + "\" class=\"img-thumbnail\"  width=\"100\" height=\"100\"></a>";
+
+		var fila = "<td>" + id + "</td><td>" + img + "</td><td>$" + precio + "</td><td>" + titulo + "</td><td>" + descripcion + "</td><td>" + categoria + "</td> <td>" + boton + "</td>";
 
 		var btn = document.createElement("tr");
 		btn.innerHTML = fila;
-		let tabla=document.getElementById("tablita");
+		let tabla = document.getElementById("tablita");
 		tabla.appendChild(btn);
 		console.log(fila);
 	}
 }
+
+function obtenerProductos2() {
+	fetch('http://127.0.0.1:3000/muebleria')
+		.then(res => res.json())
+		.then(data => {
+			productos = data;
+			productos.forEach(
+				function (producto) {
+					producto.precio = parseFloat(producto.precio)
+				});
+			guardar2(productos);
+			console.log('Si paso por aqui : tamaño ' + productos.length + " la ide: " + productos[2].id);
+			$(document).ready(function () {
+				$('#tabla').DataTable({
+					language: {
+						search: "Buscar:",
+						lengthMenu: "Desplegar _MENU_ elementos",
+						info: "Mostrando del elemento _START_ al _END_ , de _TOTAL_ elementos en total",
+						infoEmpty: "No hay elementos por mostrar",
+						infoFiltered: "(se filtraron _MAX_ elementos en total)",
+						infoPostFix: "",
+						loadingRecords: "Cargando...",
+						zeroRecords: "No se encuentran registros",
+						emptyTable: "No hay datos disponible en la tabla",
+						paginate: {
+							first: "Primera",
+							previous: "Anterior",
+							next: "Siguiente",
+							last: "Ultima"
+						},
+						aria: {
+							sortAscending: ": ordenar de forma ascendente",
+							sortDescending: ": ordenar de forma descendente"
+						}
+					}
+				});
+			});
+
+
+		})
+		.catch(error => {
+			alert(error);
+		});
+}
+
+function guardar2(productos) {
+
+	for (i = 0; i < productos.length; i++) {
+		var id = productos[i].id;
+		var precio = productos[i].precio.toFixed(2);
+		var categoria = productos[i].categoria;
+		var titulo = productos[i].titulo;
+		var fila = "<td>" + id + "</td><td>$" + precio + "</td><td>" + titulo + "</td><td>" + categoria + "</td> <td><a href= \" " + productos[i].id + "\"class=\"btn btn-warning\">Actualizar</a><a class=\"btn btn-danger\" onclick=\"ELiminar(" + productos[i].id + ")\">Eliminar</a></td>";
+
+		var btn = document.createElement("tr");
+		btn.innerHTML = fila;
+		let tabla = document.getElementById("tablita");
+		tabla.appendChild(btn);
+	}
+}
+
+
 
 
