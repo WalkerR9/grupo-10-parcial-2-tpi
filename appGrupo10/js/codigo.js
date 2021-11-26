e9cc44bbd6fc165b18f081467d8f6ce41ed7eaa8
Moises Molina <mo18022@ues.edu.sv>
2021-11-25 22:36:22 -0600

    subida de Final 1 sin imagenes

--- appGrupo10/js/codigo.js
+++ appGrupo10/js/codigo.js
@@ -119,7 +119,8 @@ function guardar2(productos) {
 		var precio = productos[i].precio.toFixed(2);
 		var categoria = productos[i].categoria;
 		var titulo = productos[i].titulo;
-		var fila = "<td>" + id + "</td><td>$" + precio + "</td><td>" + titulo + "</td><td>" + categoria + "</td> <td><a href= \" " + productos[i].id + "\"class=\"btn btn-warning\">Actualizar</a><a class=\"btn btn-danger\" onclick=\"ELiminar(" + productos[i].id + ")\">Eliminar</a></td>";
+		let btnActualizar = "	<form action=\"actualizar.html\" method=\"get\"><input type=\"hidden\" name=\"id\" value=\"" + productos[i].id + "\"><button type=\"submit\" class=\"btn btn-primary\">Actualizar</button></form>";
+		var fila = "<td>" + id + "</td><td>$" + precio + "</td><td>" + titulo + "</td><td>" + categoria + "</td> <td>" + btnActualizar + "  <a class=\"btn btn-danger\" onclick=\"borrarMueble(" + productos[i].id + ")\">Eliminar</a></td>";
 
 		var btn = document.createElement("tr");
 		btn.innerHTML = fila;
@@ -129,5 +130,64 @@ function guardar2(productos) {
 }
 
 
+function addMueble() {
+	let producto = {
+		titulo: document.getElementById("titulo").value,
+		precio: document.getElementById("precio").value,
+		descripcion: document.getElementById("descripcion").value,
+		categoria: document.getElementById("categoria").value,
+	}
+	let data = JSON.stringify(producto);
+
+	fetch('http://localhost:3000/muebleria', {
+		method: 'POST', // or 'PUT'
+		body: data, // data can be `string` or {object}!
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	}).then(res => {
+		res.json();
+		window.location.href = "/admin.html";
+	}).catch(error =>
+			console.error('Error:', error)
+	);
+
+
+}
+
+
+function borrarMueble(id) {
+	fetch('http://localhost:3000/muebleria' + '/' + id, {
+		method: 'delete'
+	})
+		.then(response => {
+			response.json();
+			location.reload();
+		});
+}
+
+function getMueble() {
+	const queryString = window.location.search;
+	const urlParams = new URLSearchParams(queryString);
+	var product = urlParams.get('id');
+	console.log("este es el producto: " + product);
+
+	fetch('http://127.0.0.1:3000/muebleria/' + product)
+		.then(res => res.json())
+		.then(data => {
+			setProducto(data);
+		})
+		.catch(error => {
+			alert(error);
+		});
+
+	function setProducto(data) {
+		document.getElementById('id').value = data.id;
+		document.getElementById('titulo').value = data.titulo;
+		document.getElementById('precio').value = data.precio;
+		document.getElementById('descripcion').value = data.descripcion;
+		document.getElementById('categoria').value = data.categoria;
+	}
+}
 
 
